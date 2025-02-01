6ac92d93845eb501b5ba472fbf42a19f288fe5fb
lewis njuw <lewiskinyuanjue.ke@gmail.com>
2025-02-01 11:23:28 +0000

    ui for main comonent

--- gemin-clone/src/components/main/Main.jsx
+++ gemin-clone/src/components/main/Main.jsx
@@ -31,6 +31,19 @@ export const Main = () => {
                     <img src={assets.code_icon} alt="" />
                 </div>
             </div>
+            <div className="main-bottom">
+                <div className="search-box">
+                    <input type="text" placeholder='Enter prompt ' />
+                    <div>
+                        <img src={assets.gallery_icon} alt="" />
+                        <img src={assets.mic_icon} alt="" />
+                        <img src={assets.send_icon} alt="" />
+                    </div>
+                </div>
+                <p className='bottom-info'>
+                    Gemini may display incorrect information 
+                </p>
+            </div>
         </div>
     </div>
   )
